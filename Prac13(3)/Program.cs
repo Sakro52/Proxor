211ccde583161f95e18c04762a891719ffad6c62
Sakro52 <mrzambak484@gmail.com>
2024-11-26 12:13:30 +0300

    Add files via upload

---
+++ Prac13(3)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac13_3_
+{
+    internal class Program
+    {/// <summary>
+     /// 
+     /// </summary>
+     /// <param name="args"></param>
+     /// Задача 3. Найти s=min(a, b) + min(c, d), используя вспомогательные функции (ввода аргументов, нахождения минимальной величины, вывода результата).
+     /// 
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        static int Input(string messenger)
+        {
+            Console.Write(messenger);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int a = Input("a=");
+            int b = Input("b=");
+            int c = Input("c=");
+            int d = Input("d=");
+            int s = Min (a,b) + Min (d,c);
+            Console.WriteLine($"{s}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practicu,/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicu_
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        ///Задача 1.Найти минимальную величину из двух целых переменных a, b, используя вспомогательные методы
+        //(ввода аргументов, нахождения минимальной величины, вывода результата).
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("x=");
+            int x =int.Parse(Console.ReadLine());
+            Console.Write("y=");
+            int y =int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Min(x,y)}");
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
