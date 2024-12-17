bab9285309d6c9cd7856d2c249369d635ff96264
Sakro52 <mrzambak484@gmail.com>
2024-12-17 14:20:12 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // определите текущее количество элементов коллекции;
+        static void El(int a)
+        {
+            Console.WriteLine(a);
+         }
+            // преобразуйте все буквы элементов в прописные;
+            static void Caps(List<string> list)
+        {
+            foreach (var el in list)
+            {
+                Console.WriteLine(el.ToUpper());
+            }
+            Console.WriteLine();
+        }
+        
+        static void Output(List<string> list)
+        {
+            foreach (var el in list)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine();
+        }
+            static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>(){ "Бугати", "БМВ", "Мерседес", "Лотус", "Тоёта", "Лада" };
+
+            // добавьте в список элементы строчными буквами; 
+            // выведите содержимое коллекции на экран;
+            list.Add("шевроле");
+            Output(list);
+
+            // определите текущее количество элементов коллекции;
+            El(list.Count);
+
+            // преобразуйте все буквы элементов в прописные;
+            Caps(list);
+
+            // добавьте в коллекцию новый элемент в конец списка соответствующего варианта;
+            list.Add("Ферари");
+            Output(list);
+
+            // добавьте в коллекцию новый элемент в начале списка соответствующего варианта;
+            // выведите содержимое коллекции на экран;
+            list.Insert(0,"Порш");
+            Output(list);
+
+            // удалите третий элемент коллекции; выведите содержимое коллекции на экран;
+            list.RemoveAt(3);
+            Output(list);
+
+            // расположите элементы в обратном порядке
+            list.Reverse();
+            Output(list);
+            Console.ReadKey();
+        }
+    }
+}
