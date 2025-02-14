c2a9e24dc51288834ac3df5e8137d529ba19f7af
Sakro52 <mrzambak484@gmail.com>
2025-02-14 12:17:29 +0200

    Add files via upload

---
+++ p22/Calculate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+
+    internal class Calculate
+    {
+        
+        /// <summary>
+        /// Объявление полей
+        /// </summary>
+        private double field1;
+        private double field2;
+
+        public double Field1 { get => field1; set { if (value < 0) throw new ArgumentException("Error");field1 = value; } }
+        public double Field2 { get => field2; set { if (value < 0) throw new ArgumentException("Error");field2 = value; } }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Calculate() { }
+        /// <summary>
+        /// Конструктор с 1 параметром
+        /// </summary>
+        /// <param name="field1"></param>
+        
+        public Calculate(double field1)
+        {
+            this.Field1 = field1;
+
+        }
+        /// <summary>
+        ///  Конструктор с 2 параметрами
+        /// </summary>
+        /// <param name="field1"></param>
+        /// <param name="field2"></param>
+        public Calculate(double field1, double field2) : this(field1)
+        {
+            this.Field2 = field2;
+        }
+        /// <summary>
+        /// Функция формирования строки с информацией об объекте
+        /// </summary>
+        //public void Output()
+        //{
+        //    Console.WriteLine($"field1 = {field1}, field2 = {field2}");
+        //}
+        public override string ToString()
+        {
+            return $"field1 = {Field1}, field2 = {Field2}";
+        }
+        /// <summary>
+        ///  Функция вычисления квадратного поля
+        /// </summary>
+        /// <returns>field1,field2</returns>
+        public double CalculateSquareRoot()
+        {
+            return Math.Sqrt(Field1 * Field2);
+        }
+
+
+    }
+}
---
+++ p22/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //объект с именем calc,конструктор без параметров
+            Calculate calc = new Calculate();
+            Console.WriteLine($"{calc.ToString()}\n");
+            calc.Field1 = 3;
+            calc.Field2 = 10;
+            Console.WriteLine($"{calc.ToString()}\n");
+            //конструктор с одним параметрами 
+            Calculate calc2 = new Calculate(4);
+            Console.WriteLine($"{calc2.ToString()}\n");
+            //конструктор с двумя параметрами
+            Calculate calc3 = new Calculate(2, 3);
+            Console.WriteLine($"{calc3.ToString()}\n");
+            Console.WriteLine($"{calc3.CalculateSquareRoot()}\n");
+
+
+            Console.WriteLine("Введите 1 число");
+            double field1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите 2 число");
+            double field2 = double.Parse(Console.ReadLine());
+            Calculate calc4 = new Calculate(field1,field2);
+            Console.WriteLine($"{calc4.CalculateSquareRoot()}\n");
+            Console.ReadLine();
+            
+        }
+    }
+}
