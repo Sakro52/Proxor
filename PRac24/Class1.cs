f64870590f1260adfca8c66389661865f87555c7
Sakro52 <mrzambak484@gmail.com>
2025-02-20 11:08:38 +0200

    Add files via upload

---
+++ PRac24/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Avto
+    {
+        private string brand;   
+        private string color;   
+        private double skor; 
+
+        
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }  
+        }
+
+      
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }  
+        }
+        public double Skor
+        {
+            get { return skor; }
+            set
+            {
+                if (value < 20)
+                    skor = 20;
+                else if (value > 120)
+                    skor = 120;  
+                else
+                    skor = value;
+              
+            }
+
+        }
+        /// <summary>
+        /// метод вывода
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Марка: {brand}, Цвет: {color}, Скорость: {skor} км/ч");
+        }
+       /// <summary>
+       /// с параметром 
+       /// </summary>
+       /// <param name="brand"></param>
+       /// <param name="color"></param>
+       /// <param name="skor"></param>
+        public Avto(string brand, string color, double skor)
+        {
+            this.Brand = brand;
+            this.Color = color;
+            this.Skor = skor;
+        }
+        /// <summary>
+        /// без параметров
+        /// </summary>
+        public Avto() { }
+    }
+
+
+
+
+}
+
---
+++ PRac24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto car1 = new Avto();
+            car1.Print();
+
+            Avto car2 = new Avto("Лада Жигули 4", "Синий", 70);
+
+            car2.Print();
+        
+            Console.Write("Введите марку автомобиля: ");
+            string brand = Console.ReadLine();
+            Console.Write("Введите цвет автомобиля: ");
+            string color = Console.ReadLine();
+            Console.Write("Введите скорость автомобиля: ");
+            double skor = double.Parse(Console.ReadLine());
+
+            Avto car3 = new Avto(brand, color, skor);
+            car3.Print();
+            Console.Read();
+        }
+    }
+}
