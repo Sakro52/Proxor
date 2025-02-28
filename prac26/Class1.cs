79b075aaf596b6a17f415be1bd41075b0845f26a
Sakro52 <mrzambak484@gmail.com>
2025-02-28 12:46:10 +0200

    Add files via upload

---
+++ prac26/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac26
+{
+    using System;
+
+    public class Person
+    {
+        private int age;
+
+        
+        private static int retirementAge;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int RetirementAge
+        {
+            get { return retirementAge; }
+            set { retirementAge = value; }
+        }
+
+        public Person() { }
+
+        public Person(int age, int retirementAge)
+        {
+            this.Age = age;
+            this.RetirementAge = retirementAge;  
+        }
+      
+        public string CheckAge()
+        {
+            if (age >= retirementAge)
+            {
+                return "На пенсии";
+            }
+            else
+            {
+                return $"До пенсии осталось {retirementAge - age} лет";
+            }
+        }
+
+    
+        public int RetirementAge1
+        {
+            get { return retirementAge; }
+            set
+            {
+                if (value >= 1 && value <= 100)
+                {
+                    retirementAge = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверное значение пенсионного возраста. Должно быть от 1 до 100 лет.");
+                }
+            }
+        }
+
+        
+        public int GetAge()
+        {
+            return age;
+        }
+
+      
+        public void SetAge(int age)
+        {
+            if (age >= 0)
+            {
+                this.age = age;
+            }
+            else
+            {
+                Console.WriteLine("Возраст не может быть отрицательным.");
+            }
+        }
+
+      
+        public static string CheckRetirementStatus(Person person)
+        {
+            if (person.age >= person.RetirementAge)
+            {
+                return "На пенсии";
+            }
+            else
+            {
+                return $"До пенсии осталось {person.RetirementAge - person.age} лет";
+            }
+        }
+    }
+}
---
+++ prac26/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Person person1 = new Person(50, 65);
+                Console.WriteLine(person1.CheckAge());   
+
+                
+                person1.RetirementAge = 60;
+                Console.WriteLine(person1.CheckAge()); 
+
+               
+                Person person2 = new Person(65, 65);
+                Console.WriteLine(Person.CheckRetirementStatus(person2)); 
+            Console.ReadKey();
+            }
+        }
+    }
+
