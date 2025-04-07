ef4c914d0aeb7208bca2bec1e2dcd90a607d974d
Sakro52 <mrzambak484@gmail.com>
2025-04-07 11:11:18 +0300

    Add files via upload

---
+++ курсачь/курсачь/PostOffice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace курсачь
+{
+    class PostOffice
+    {
+        public int Number { get; set; }
+        public string Address { get; set; }
+        public List<Newspaper> Newspapers { get; set; }
+
+        public PostOffice()
+        {
+            Newspapers = new List<Newspaper>();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Отделение №{Number}, Адрес: {Address}");
+            Console.WriteLine("Газеты:");
+            foreach (Newspaper n in Newspapers)
+            {
+                Console.WriteLine($"- {n.Name}");
+            }
+        }
+    }
+}
