c622e7d9020eb4ce042d8319fd642232aaec18cb
Sakro52 <mrzambak484@gmail.com>
2025-05-10 20:02:46 +0300

    Add files via upload

---
+++ Kursah Файлыtxt/PrintingHouse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursah
+{
+    internal class PrintingHouse
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public List<Newspaper> Newspapers { get; set; }
+
+            public PrintingHouse()
+            {
+                Newspapers = new List<Newspaper>();
+            }
+        }
+    }
+
